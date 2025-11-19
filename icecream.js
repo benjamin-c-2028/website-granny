6542d6c1579dffee9aed73475cef8ec2b83f4d52
benjamin-c-2028 <231078543+benjamin-c-2028@users.noreply.github.com>
2025-11-19 13:58:07 -0800

    yes

---
+++ icecream.js
@@ -0,0 +1,13 @@
+function allergie_function() {
+    document.getElementById("allergies").style.color = "red";
+}
+
+function myFunction() {
+  let x = document.getElementById("myInput").value;
+  document.getElementById("grannies").innerHTML = x + " grannys";
+}
+
+function selector() {
+  let x = document.getElementById("mySelect").value;
+  document.getElementById("demo").innerHTML = "put " + x + " in oven";
+}
